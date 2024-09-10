c4ef5a31f1c7f9d55856f2c128366ea6aac5fb42
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-09-10 22:47:49 +0300

    Inheritance Lab

--- lib/testing/user_test.py
+++ lib/testing/user_test.py
@@ -2,14 +2,34 @@
 
 from user import User
 
-class TestUser:
-    '''Class "User" in user.py'''
+# lib/testing/user_test.py
+import pytest
+from lib.teacher import Teacher
+from lib.student import Student
+from lib.user import User
 
-    def test_is_class(self):
-        '''is a class.'''
-        assert(object in User.__bases__)
+def test_user_initialization():
+    user = User("Jordan", "Zippy")
+    assert user.first_name == "Jordan"
+    assert user.last_name == "Zippy"
 
-    def test_initializes_with_names(self):
-        '''initializes with first and last name.'''
-        my_user = User("My", "User")
-        assert((my_user.first_name, my_user.last_name) == ("My", "User"))
+def test_teacher_initialization():
+    teacher = Teacher("Olivia", "Spencer")
+    assert teacher.first_name == "Olivia"
+    assert teacher.last_name == "Spencer"
+
+def test_teacher_teach():
+    teacher = Teacher("Olivia", "Spencer")
+    knowledge_taught = teacher.teach()
+    assert knowledge_taught in teacher.knowledge
+
+def test_student_initialization():
+    student = Student("Bruno", "Johnson")
+    assert student.first_name == "Bruno"
+    assert student.last_name == "Johnson"
+    assert student.knowledge == []
+
+def test_student_learn():
+    student = Student("Bruno", "Johnson")
+    student.learn("OOP in Python")
+    assert "OOP in Python" in student.knowledge
